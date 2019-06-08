e6da3d766fec4a1f273c0906d519409ec59c9f06
Thiago Spindola <thiagorspindola@hotmail.com>
2019-06-08 18:31:06 -0300

    Recognizing card

--- app/src/main/java/br/com/autopass/vegastps530/SerialDeviceManager.kt
+++ app/src/main/java/br/com/autopass/vegastps530/SerialDeviceManager.kt
@@ -1,27 +1,33 @@
 package br.com.autopass.vegastps530
 
 import android.app.Activity
+import android.os.Handler
 import android.util.Log
+import com.telpo.tps550.api.TimeoutException
 import com.telpo.tps550.api.nfc.Nfc
 import com.telpo.tps550.api.reader.SmartCardReader
+import io.reactivex.Completable
+import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
-class SerialDeviceManager(ctx: Activity){
+class SerialDeviceManager(ctx: Activity) {
     private val slotSAM = 0
     private val slotPSAM = 1
     private val slotNFC = 5
     private val HEX_CHARS = "0123456789ABCDEF"
     private val nfc = Nfc(ctx)
-    private val reader = SmartCardReader(ctx,slotSAM)
+    private val reader = SmartCardReader(ctx, slotSAM)
 
-    fun open(){
+    fun open() {
         val disposable = Single.create<Boolean> { emitter ->
             val ret = openReader()
             emitter.onSuccess(ret)
 
         }
-            .subscribe{t->
-                if(t)
+            .subscribe { t ->
+                if (t)
                     openNFC()
                 else
                     closeNFC()
@@ -30,24 +36,24 @@ class SerialDeviceManager(ctx: Activity){
         disposable.dispose()
     }
 
-    private fun openReader():Boolean{
+    private fun openReader(): Boolean {
         Log.d("READER_LIB", "Opening reader")
         return reader.open()
     }
 
-    private fun openNFC(){
+    private fun openNFC() {
         Log.d("READER_LIB", "Opening NFC")
         nfc.open()
         reader.iccPowerOn()
     }
 
-    private fun closeNFC(){
+    private fun closeNFC() {
         Log.d("READER_LIB", "Closing NFC")
         nfc.close()
         reader.iccPowerOff()
     }
 
-    private fun resetDevice(slot: Int):ByteArray{
+    private fun resetDevice(slot: Int): ByteArray {
         return if (slot == slotNFC) {
             nfc.activate(50)
 
@@ -56,7 +62,7 @@ class SerialDeviceManager(ctx: Activity){
         }
     }
 
-    private fun sendCommandToSAM(slot: Int, apdu: ByteArray):ByteArray{
+    private fun sendCommandToSAM(slot: Int, apdu: ByteArray): ByteArray {
         return if (slotPSAM == slot) {
             reader.transmit(apdu)
         } else {
@@ -64,28 +70,49 @@ class SerialDeviceManager(ctx: Activity){
         }
     }
 
-    private fun readCardSerialNumber():ByteArray?{
-        var nfcCardID: ByteArray? = null
-        val nfcData = nfc.activate(50)
-        if (nfcData != null && nfcData.size >= 6) {
-            nfcCardID = ByteArray(nfcData[5].toInt())
-            System.arraycopy(nfcData, 6, nfcCardID, 0, nfcCardID.size)
-        }
-        return nfcCardID
+    private fun isCardPresent(): Boolean {
+        val nfcData = nfc.activate(30)
+        return (nfcData != null && nfcData.size >= 6)
     }
 
-    fun String.hexStringToByteArray() : ByteArray {
+    fun String.hexStringToByteArray(): ByteArray {
 
         val result = ByteArray(length / 2)
 
         for (i in 0 until length step 2) {
             val firstIndex = HEX_CHARS.indexOf(this[i])
             val secondIndex = HEX_CHARS.indexOf(this[i + 1])
-
             val octet = firstIndex.shl(4).or(secondIndex)
             result[i.shr(1)] = octet.toByte()
         }
 
         return result
+    }
+
+    private fun readBalance(){
+
+    }
+
+    private fun readCard(b: Boolean) {
+        if(b){
+
+        }
+        else{
+            Log.d("READER_LIB", "Waiting card")
+        }
+    }
+
+    fun reader() {
+        var ret = false
+        val disposable = Observable.fromCallable {
+            try {
+                ret = isCardPresent()
+            } catch (t: TimeoutException) {
+            }
+        }
+            .repeatUntil { ret }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { readCard(ret) }
     }
 }
